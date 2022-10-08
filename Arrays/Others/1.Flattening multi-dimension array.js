533f1ff8421a52af9e35ea613398996973fc9ac3
Uttam Marandi <uttammarandi007@gmail.com>
2022-10-08 11:15:19 +0530

    chore : flat multidimension array using .flat(Infinity)

--- Arrays/Others/1.Flattening multi-dimension array.js
+++ Arrays/Others/1.Flattening multi-dimension array.js
@@ -9,3 +9,10 @@ const flatenArray = (arr) => {
 const multiDimensionalArr = [11, [22, 33], [44, [55, 66, [77, [88]], 99]]];
 const flatArr = flatenArray(multiDimensionalArr);
 console.log("flatArr:", flatArr);
+
+const multiDimensionalArr2 = [11, [22, 33], [44, [55, 66, [77, [88]], 99]]];
+
+const flatArray2 = multiDimensionalArr2.flat(Infinity);
+console.log("flatArray2:", flatArray2);
+
+// We can also pass number values into .flat() instead of Infinity to define level of flatness
