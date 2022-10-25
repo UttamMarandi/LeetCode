ff0ace308f5a53d14389207ddd37556f6f79c80c
Uttam Marandi <uttammarandi007@gmail.com>
2022-10-25 23:10:56 +0530

    chore : find diff between two arrays improved

--- Js-Coding-Practice/2.Difference between two arrays.js
+++ Js-Coding-Practice/2.Difference between two arrays.js
@@ -13,3 +13,21 @@ const a2 = ["a", "b", "c", "d"];
 
 const diff = a2.filter((item) => !a1.includes(item));
 console.log("diff:", diff);
+
+const findDiff = (a1, a2) => {
+  if (a1.length > a2.length) {
+    return a1.filter((item) => a2.includes(item));
+  } else {
+    return a2.filter((item) => a1.includes(item));
+  }
+};
+
+const diff3 = findDiff(a1, a2);
+console.log("diff3:", diff3);
+
+const diff4 = findDiff(a2, a1);
+console.log("diff4:", diff4);
+
+const diff2 = a1.filter((item) => !a2.includes(item));
+
+console.log("diff2:", diff2);
