0acc0270472e277a74b57ae6273cc5b04a9c2807
Uttam Marandi <uttammarandi007@gmail.com>
2022-11-02 22:50:34 +0530

    chore : check if string is created using object literal or String object

---
+++ Js-Coding-Practice/20.Test string as a literal and as an object.js
@@ -0,0 +1,30 @@
+// Q. Write a function which will test string as a literal and as an object?
+// Example 01:
+
+// Input:  const ltrlStr = "Hi I am string literal";
+// Output: It is a string literal
+//  Save
+// Example 02:
+
+// Input:  const objStr = new String("Hi I am string object");
+// Output: It is an object of string
+
+const ltlrStr = "Hi I am string literal";
+const objStr = new String("Hi I am a string object");
+
+const checkType = (str) => {
+  if (str instanceof String) {
+    console.log(objStr);
+    return `instace string ${objStr}`;
+  } else if (typeof str === "string") {
+    console.log(ltlrStr);
+    return `liteal string ${ltlrStr}`;
+  } else {
+    console.log("Some other type");
+    return `Some Other type`;
+  }
+};
+
+console.log(checkType(ltlrStr));
+
+console.log(checkType(objStr));
