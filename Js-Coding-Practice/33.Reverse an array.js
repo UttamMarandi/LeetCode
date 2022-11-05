5d36929dce176846d1326fe57e5b7328382a6469
Uttam Marandi <uttammarandi007@gmail.com>
2022-11-05 19:46:04 +0530

    chore : reverse an array

---
+++ Js-Coding-Practice/33.Reverse an array.js
@@ -0,0 +1,9 @@
+// Write a function to reverse an array?
+
+const myArray = [10, 20, 30, 40];
+const myArray2 = ["Uttam", "Hairsh", "Kishor", "Mahesh"];
+
+const reverseArray = myArray.reverse();
+console.log("reverseArray:", reverseArray);
+
+console.log(myArray2.reverse());
