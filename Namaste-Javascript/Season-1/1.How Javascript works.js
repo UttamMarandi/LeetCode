1476a2da1e858a26ac0634b916ad586cc4844bb7
Uttam Marandi <uttammarandi007@gmail.com>
2022-11-09 23:09:46 +0530

    chore : How js works

---
+++ Namaste-Javascript/Season-1/1.How Javascript works.js
@@ -0,0 +1,10 @@
+//
+// Everything is javascript happens inside an execution context
+
+// All js code is executed inside this execution context.
+
+// Execution context has two components :
+//1. Memory Component : All the variables and functions are stored as key value pairs. Also called Variable enviroment
+//2. Code component : All the code is executed in the code component one line at a time. It is also known as thread of execution.
+
+// Javascript is a synchronous single-threaded language. Jacascript can execute code one line at a time in a particular order that is top to bottom
