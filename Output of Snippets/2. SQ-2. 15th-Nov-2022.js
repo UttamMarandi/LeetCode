d68449c71e3f9bf591d064b5bf1b1efee3c63ea2
Uttam Marandi <uttammarandi007@gmail.com>
2022-11-16 23:49:02 +0530

    chore : slice , splice, some, filter

--- Output of Snippets/2. SQ-2. 15th-Nov-2022.js
+++ Output of Snippets/2. SQ-2. 15th-Nov-2022.js
@@ -89,6 +89,9 @@
   delete objA.foo;
   console.log(objA.foo);
   console.log(objB.foo);
+  delete objA.__proto__.foo;
+  console.log(objA.foo);
+  console.log(objB.foo);
 })();
 
 // var objA = Object.create({
@@ -97,3 +100,134 @@
 // creates an object objA with objA.__proto__ (its prototype) set to {foo: 'foo'}. There is no actual objA.foo property, it is actually objA.__proto__.foo, so nothing is deleted.
 // If you do instead
 // var objA = {foo: 'foo'}
+
+// Code 79
+
+(function () {
+  var objA = {
+    foo: "foo",
+  };
+  var objB = objA;
+  objB.foo = "bar";
+
+  delete objA.foo;
+  console.log(objA.foo);
+  console.log(objB.foo);
+})();
+
+// Code 80
+(function () {
+  var array = new Array("100");
+  console.log(array);
+  console.log(array.length);
+})();
+
+// Code 81
+(function () {
+  var array1 = [];
+  var array2 = new Array(100); // passing integer as argument means , we are creating an array of size 100
+  var array3 = new Array(["1", 2, "3", 4, 5.6]);
+  console.log(array1);
+  console.log(array2);
+  console.log(array3);
+  console.log(array3.length);
+})();
+
+// Code 82
+(function () {
+  var array = new Array("a", "b", "c", "d", "e");
+  array[10] = "f";
+  console.log("array:", array);
+  delete array[10];
+  console.log("array:", array);
+  console.log(array.length);
+})();
+
+// Code 83
+(function () {
+  var animal = ["cow", "horse"];
+  animal.push("cat");
+  animal.push("dog", "rat", "goat");
+  console.log(animal.length);
+})();
+
+// Code 84
+(function () {
+  var animal = ["cow", "horse"];
+  animal.push("cat");
+  animal.unshift("dog", "rat", "goat");
+  console.log(animal);
+})();
+
+// Code 85
+(function () {
+  var array = [1, 2, 3, 4, 5];
+  console.log(array.indexOf(2));
+  console.log([{ name: "John" }, { name: "John" }].indexOf({ name: "John" })); // Doubt => Maybe indexOf can't compare objects.
+  console.log([[1], [2], [3], [4]].indexOf([3])); // Doubt => Maybe indexOd can't comapare objects and array is an object
+  console.log("abcdefgh".indexOf("e"));
+})();
+
+// Code 86
+(function () {
+  var array = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 6];
+  console.log(array.indexOf(2));
+  console.log(array.indexOf(2, 3)); // indexOf(searchElement, fromIndex)
+  console.log(array.indexOf(2, 10));
+})();
+
+// Code 87
+(function () {
+  var numbers = [2, 3, 4, 8, 9, 11, 13, 12, 16];
+  var even = numbers.filter(function (element, index) {
+    return element % 2 === 0;
+  });
+  console.log(even);
+
+  var containsDivisibleby3 = numbers.some(function (element, index) {
+    // some returns a boolean value
+    return element % 3 === 0;
+  });
+
+  console.log(containsDivisibleby3);
+})();
+
+// Code 88 // Doubt
+(function () {
+  var containers = [2, 0, false, "", "12", true];
+  var containers = containers.filter(Object);
+  console.log(containers);
+  var containers = containers.filter(Number);
+  console.log(containers);
+  var containers = containers.filter(String);
+  console.log(containers);
+  var containers = containers.filter(Boolean); // Boolean means true
+  console.log(containers);
+})();
+
+// Code 89
+
+(function () {
+  var list = ["foo", "bar", "john", "ritz"]; //.slic() return new array. .slice(startIndex, endIndex)
+  console.log(list.slice(1));
+  console.log(list.slice(1, 3));
+  console.log(list.slice());
+  console.log(list.slice(2, 2));
+  console.log(list);
+})();
+
+// Codr 90
+
+(function () {
+  var list = ["foo", "bar", "john"];
+  console.log(list.splice(1));
+  console.log(list.splice(1, 2));
+  console.log(list);
+})();
+
+// Code 91
+(function () {
+  var arrayNumb = [2, 8, 15, 16, 23, 42];
+  arrayNumb.sort(); // Numbers are sorted as strings. That's why 1s get priority
+  console.log(arrayNumb);
+})();
